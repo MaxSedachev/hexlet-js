f9dd40f1a0a8709ab15343eed0ee576bd6f9c824
msedachev <msedachev@mts.ru>
2024-05-26 12:11:50 +0300

    added option with assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,23 +1,28 @@
 import { capitalize } from '../src/capitalize.js';
+import {strict as assert} from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+// Варианты с assert:
+
+// assert.notStrictEqual(capitalize(1234), 1234)
+// assert.notStrictEqual(capitalize(null), null)
+
+// Варианты с if:
 
 // if (capitalize(1234) !== 1234) {
 //     throw new Error('Функция работает неверно!');
 //   }
-
 // if (capitalize(null) !== null) {
 //     throw new Error('Функция работает неверно!');
 //   }
-
-if (capitalize('hello') !== 'hello') {
-    throw new Error('Функция работает неверно!');
-  }
 
 console.log('Все тесты пройдены!');
